716096f1640873323c702e7d4fcb083fc37cf72c
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-04-19 16:00:43 -0700

    no need to use styled to modify Stack because it does nothing
    no need to use styled to modify Stack because it does nothing

--- src/components/reusable/Logo.jsx
+++ src/components/reusable/Logo.jsx
@@ -1,26 +1,31 @@
 import Stack from "@mui/material/Stack";
-import IconButton from '@mui/material/IconButton';
-import MapsHomeWorkSharpIcon from '@mui/icons-material/MapsHomeWorkSharp';
-import { Typography } from '@mui/material';
-import {styled} from '@mui/material';
+import IconButton from "@mui/material/IconButton";
+import MapsHomeWorkSharpIcon from "@mui/icons-material/MapsHomeWorkSharp";
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material";
 function Logo() {
-    const LogoStack = styled(Stack)(
-      {padding: 0}
-    )
-    return (<>        
-    <LogoStack direction= "row">
-      <IconButton edge='start' color = 'inherit' aria-label='logo'  sx={{ flexGrow: 1, display: { xs: 'none', sm:'flex'} }}  >
-          <MapsHomeWorkSharpIcon style={{fontSize:48}} />
-      </IconButton>
-      <LogoStack sx={{ flexGrow: 1, display: { xs: 'none', sm:'none', md: 'flex'} }}>
-        <Typography variant='h6'  sx={{letterSpacing:'1px'}}>
-        BUILD SCHOOL
+  return (
+    <>
+      <Stack direction="row">
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="logo"
+          sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}
+        >
+          <MapsHomeWorkSharpIcon style={{ fontSize: 48 }} />
+        </IconButton>
+        <Stack
+          sx={{ flexGrow: 1, display: { xs: "none", sm: "none", md: "flex" } }}
+        >
+          <Typography variant="h6" sx={{ letterSpacing: "1px" }}>
+            BUILD SCHOOL
           </Typography>
-        <Typography variant='h6'>
-        For India Children
-          </Typography>      
-      </LogoStack>
-      </LogoStack></>  );
+          <Typography variant="h6">For India Children</Typography>
+        </Stack>
+      </Stack>
+    </>
+  );
 }
 
 export default Logo;

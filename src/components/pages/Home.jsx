37d0cf460785967164e208296848a4da6279eb4e
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-04-24 15:27:29 -0700

    center description and picture cards

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,39 +1,36 @@
 import React from "react";
-import givealittlehelpalot from "../../assets/givealittlehelpalot.jpg";  //1500X800px  
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/system';
-import Typography  from "@mui/material/Typography";
-import Grid  from "@mui/material/Grid";
+import givealittlehelpalot from "../../assets/givealittlehelpalot.jpg"; //1500X800px
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 import CardCategory from "../reusable/CardCategory.jsx";
 import CardImage from "../reusable/CardImage";
 import categoryItems from "../../assets/data/data.json";
 import AlbumCategory from "../reusable/AlbumCategory";
 //import CategoryImageData from "../reusable/CategoryImageData";
-const StyledPaper = styled(Paper)({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderRadius: '20px',
-  overflow: 'hidden',
-  // backgroundImage: `url(${image})`
-});
+// const StyledPaper = styled(Paper)({
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   borderRadius: "20px",
+//   overflow: "hidden"
+//   // backgroundImage: `url(${image})`
+// });
 
-const StyledImage = styled('img')({
-  width: '100%',
-  height: 'auto',
-  //borderRadius: '20px',
-  
-});
+// const StyledImage = styled("img")({
+//   width: "100%",
+//   height: "auto"
+//   //borderRadius: '20px',
+// });
 
 function Home() {
   return (
-    <>      
-   
-  
-   {/* <StyledPaper>
+    <>
+      {/* <StyledPaper>
           
              <StyledImage src={givealittlehelpalot} alt='Give a little, help a lot' />
     </StyledPaper> 
@@ -53,9 +50,10 @@ function Home() {
    ))}
       </Grid>
       
-     */}    
-    <AlbumCategory />
-      
+     */}
+      <Box display="flex" bgcolor="lightblue">
+        <AlbumCategory />
+      </Box>
     </>
   );
 }

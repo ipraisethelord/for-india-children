ef4596976a69d1d9ef4141c831cec6e4fbc0e8c6
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-04-26 10:21:19 -0700

    centering table

--- src/components/pages/CategoryDetail.jsx
+++ src/components/pages/CategoryDetail.jsx
@@ -1,26 +1,22 @@
-import { useParams } from "react-router-dom"
-import { useContext } from 'react'
-import Context from '../reusable/Context'
+import { useParams } from "react-router-dom";
+import Stack from "@mui/material/Stack";
+//import { useContext } from 'react'
+//import Context from '../reusable/Context'
 import BasicTable from "../reusable/BasicTable";
 import Box from "@mui/material/Box";
 export default function CategoryDetail() {
-    const userData = useContext(Context)
-    const params = useParams();
-    const { id } = params;
-    const m = id.split(":")[1];
-   
-    return (
-        <Box display="flex" bgcolor="lightblue" justifyContent="center">
-        <Box sx={{ width: '100%' }}>
-            <h1>Product Details</h1>
-            <p>The product ID is {m}</p>          
-            {               
-                <>                    
-                     <BasicTable tableName={m} />
-                </>
-               
-            }
-            </Box></Box>
-        
-    )
+  // const userData = useContext(Context)
+  const params = useParams();
+  const { id } = params;
+  const m = id.split(":")[1];
+
+  return (
+    <Box display="flex" bgcolor="lightblue" justifyContent="center">
+      <Stack>
+        <h1>{m} needs details</h1>
+
+        {<BasicTable tableName={m} />}
+      </Stack>
+    </Box>
+  );
 }

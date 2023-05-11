1c4e518986033ca24b05cc63c8ff274ae8ac4692
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-05-11 14:38:24 -0700

    Update background color to alpha 30% of honeydew for VisionAndMissionPaper.jsx

--- src/components/reusable/VisionAndMissionPaper.jsx
+++ src/components/reusable/VisionAndMissionPaper.jsx
@@ -2,23 +2,24 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-
+import { alpha } from "@mui/material/styles";
+import theme from "../../styles/theme";
 export default function VisionAndMissionPaper({ item, content }) {
   return (
     <Box
       sx={{
-          display: "flex",
-          alignItems: "flex-start",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          "& > :not(style)": {
-            p: 5,
-            width: 400,
-            height: "auto"
-        }
+        display: "flex",
+        alignItems: "flex-start",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        "& > :not(style)": {
+          p: 5,
+          width: "22rem",
+          height: "auto",
+        },
       }}
     >
-      <Paper elevation={3} sx={{ backgroundColor: "honeydew" }}>
+      <Paper elevation={3} sx={{ backgroundColor: alpha("#f0fff0", 0.3) }}>
         <Typography variant="h2">{item}</Typography>
         <Typography>{content}</Typography>
       </Paper>
@@ -26,6 +27,4 @@ export default function VisionAndMissionPaper({ item, content }) {
   );
 }
 
-
 //      <VisionAndMissionPaper content="This is my world" item="Vision" />
-

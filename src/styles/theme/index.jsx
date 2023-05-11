953b24adf9db13ae471e498d84ecfda512ba22b7
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-05-11 11:07:12 -0700

    add BannerVariant to Typography in theme/index.jsx

--- src/styles/theme/index.jsx
+++ src/styles/theme/index.jsx
@@ -4,17 +4,17 @@ import { darken, lighten } from "polished";
 export const DrawerWidth = 250;
 
 export const Colors = {
-  primary: "#00a2ff",/***blue */
+  primary: "#00a2ff" /***blue */,
   secondary: "#bbdefb",
-  success: "#4CAF50", /***green */
-  info: "#45bdf9", /***blue */
-  danger: "#FF5722",/**orange */
-  warning: "#FFC107",/***yellow*/
-  dark: "#0e1b20",/***gray blackish */
+  success: "#4CAF50" /***green */,
+  info: "#45bdf9" /***blue */,
+  danger: "#FF5722" /**orange */,
+  warning: "#FFC107" /***yellow*/,
+  dark: "#0e1b20" /***gray blackish */,
   light: "#aaa",
   muted: "#abafb3",
   border: "#DDDFE1",
-  inverse: "#2b669d",/***light navy blue */
+  inverse: "#2b669d" /***light navy blue */,
   shaft: "#333",
   ///////////////
   // Grays
@@ -51,6 +51,14 @@ const theme = createTheme({
         disableElevation: true,
       },
     },
+    MuiTypography: {
+      variants: {
+        BannerTypography: {
+          fontFamily: "Nunito, sans-serif",
+          color: "#000",
+        },
+      },
+    },
     MuiTooltip: {
       defaultProps: {
         arrow: true,
@@ -67,21 +75,22 @@ const theme = createTheme({
     MuiDrawer: {
       styleOverrides: {
         paper: {
-          width: DrawerWidth,          
+          width: DrawerWidth,
           background: Colors.primary,
           color: Colors.secondary,
-          borderRadius: '0px 100px 0px 0px',
-          borderRight: `1px solid ${Colors.primary}`
-        }
-      }
+          borderRadius: "0px 100px 0px 0px",
+          borderRight: `1px solid ${Colors.primary}`,
+        },
+      },
     },
     MuiDivider: {
       styleOverrides: {
         root: {
-          borderColor: lighten(0.2, Colors.primary)
-        }
-      }
+          borderColor: lighten(0.2, Colors.primary),
+        },
+      },
     },
+
     MyShopButton: {
       styleOverrides: {
         root: {

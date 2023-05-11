974f27bf184bb6d288245f0fa5cedca0aa9e6f07
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-05-10 17:18:01 -0700

    Create VisionAndMissionPaper.jsx

---
+++ src/components/reusable/VisionAndMissionPaper.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+export deafult function VisionAndMissionPaper({ item, content }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "flex-start",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        "& > :not(style)": {
+          p: 5,
+          width: 330,
+          height: "auto"
+        }
+      }}
+    >
+      <Paper elevation={3} sx={{ backgroundColor: "honeydew" }}>
+        <Typography variant="h2">{item}</Typography>
+        <Typography>{content}</Typography>
+      </Paper>
+    </Box>
+  );
+}
+
+
+//      <VisionAndMissionPaper content="This is my world" item="Vision" />
+

bf7be12b53e9bb5303280658ef54fa385e609fdb
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-05-11 13:00:29 -0700

    Update BannerBox.jsx

--- src/components/reusable/BannerBox.jsx
+++ src/components/reusable/BannerBox.jsx
@@ -1,26 +1,34 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import theme from '../../styles/theme';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import theme from "../../styles/theme";
 export default function BannerBox() {
   return (
-    <Box 
+    <Box
       sx={{
         width: "100%",
-       
       }}
     >
-      <Paper sx={{ p: 2 , backgroundColor: theme.palette.secondary.main,
-        '&:hover': {
-          backgroundColor: theme.palette.primary.main,
-        },}}>
-        <Typography variant='Banner' sx={{ color: '#000' }}>
-          Behold, I will do a new thing; now it shall spring forth; shall ye not know it? I will even make a way in the wilderness, and rivers in the desert.
-        </Typography>
-        <Typography  variant='Banner' align="right" color ="#333">
-          Isaiah 43:19
-        </Typography>
+      <Paper
+        sx={{
+          p: 2,
+          backgroundColor: theme.palette.secondary.main,
+          "&:hover": {
+            backgroundColor: theme.palette.primary.main,
+          },
+        }}
+      >
+        <Box>
+          <Typography variant="banner" align="center">
+            Behold, I will do a new thing; now it shall spring forth; shall ye
+            not know it? I will even make a way in the wilderness, and rivers in
+            the desert.
+          </Typography>
+          <Typography variant="body1" align="right" sx={{ color: "#333" }}>
+            Isaiah 43:19
+          </Typography>
+        </Box>
       </Paper>
     </Box>
   );

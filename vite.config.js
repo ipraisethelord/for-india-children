319e53235a8c877640a7077f9f0b0e3381ce3165
ipraisethelord <127550036+ipraisethelord@users.noreply.github.com>
2023-05-15 13:16:33 -0700

    Update vite.config.js bae have to be '/', remove it causing 404 for all pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // remove '/' the base URL by setting base to an empty string, making the path relative to its deployment directory
+  base: '/', 
 })
